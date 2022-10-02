657b646ac1328f8192480e914140d66e370081f6
spring <wenchuan.lin@abagile.com>
2022-10-02 12:29:41 +0800

    Refactor version of duplicate_encoder

--- clojure/src/code_wars/duplicate_encoder.clj
+++ clojure/src/code_wars/duplicate_encoder.clj
@@ -3,31 +3,12 @@
     [clojure.string :as str]
     [clojure.test :refer [deftest testing is]]))
 
-; (defn to-chars [text]
-;   (->> (char-array text)
-;        (map str/lower-case)))
-
-(defn encode-map [chars]
-  (->> chars
-       (frequencies)
-       (reduce-kv (fn [m k v]
-                    (assoc m k (if (> v 1)
-                                 \)
-                                 \()))
-
-                  {})))
-
 (defn encode-dups [text]
-  (let [chars (map str/lower-case (char-array text))
-        encode-m (encode-map chars)]
-    (->> chars
-        (map #(get encode-m (str %)))
-        (apply str))))
-
-; (defn encode-dups2 [text]
-;   [let [chars (->> (char-array text)
-;                    (map str/lower-case))
-;         encode-m (encode-map chars)]])
+  (let [lowered (str/lower-case text)
+        freq (frequencies lowered)]
+    (->> lowered
+         (map #(if (= 1 (freq %)) \( \)))
+         (apply str))))
 
 (defn dotest [text expected]
   (is (= (encode-dups text) expected)))
@@ -41,20 +22,3 @@
     (dotest "AaBbC" "))))("))
 
   (testing "ignore-case" (dotest "Success" ")())())")))
-
-
-
-
-(comment
-  (get (encode-map "dfd") (str \d))
-  (encode-dups "dfd")
-  (apply str (char-array "dfd"))
-  (->> (frequencies (char-array "dfd"))
-       (reduce-kv (fn [m k v]
-                    (assoc m k (if (> v 1)
-                                 \)
-                                 \()))
-
-                  {}))
-
-  (map #(-> % val (> 1))))
